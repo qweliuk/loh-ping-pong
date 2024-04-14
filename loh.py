06803529280ce878c0a516fd1dfd80d534e32b19
kalekaxz <94740967+qweliuk@users.noreply.github.com>
2024-04-14 11:18:49 +0500

    Add files via upload

--- loh.py
+++ loh.py
@@ -22,39 +22,63 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
-
-class Enemy(GameSprite):
-    def update(self):
-        keys_pressed = key.get_pressed()
-        if keys_pressed[K_a] and self.rect.x > 10:
-            self.rect.x -= self.speed
-        if keys_pressed[K_d] and self.rect.x < 650:
-           self.rect.x += self.speed
-
-
-platform = Enemy('platform.png', 200, 300, 300, 150, 60)
-
-
-mach = Player('mach.png', 40, 380, 50, 150, 100)
-# speed_x = 3
-# speed_y = 3 
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys(K_UP) and self.rect_y > 5:
+            self.rect.y -= self.speed
+        if keys(K_DOWN) and self.rect_y < 500 - 80:
+            self.rect.y += self.speed  
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys(K_w) and self.rect_y > 5:
+            self.rect.y -= self.speed
+        if keys(K_s) and self.rect_y < 500 - 80:
+            self.rect.y += self.speed  
 
 
+
+
+
+racket1 = Player('platform2.png', 500, 200, 50, 150, 150)
+racket2 = Player('platform.png', 40, 200, 50, 150, 150)
+
+
+
+ball = GameSprite('mach.png',350, 250, 50, 150, 100)
+
+speed_x = 3
+speed_y = 3 
+
+finish = False
 
 
 while game:
     for e in event.get():
-        window.blit(background, (0,0))
-        mach.reset()
-        mach.update()
-        platform.reset()
-        platform.update
         if e.type == QUIT:
             game = False
-        # if finish != True: 
-        #     ball.rect.x += speed_x
-        #     ball.rect.y += speed_y
-            
+    if finish != True:
+        window.blit(background, (0,0))
+        ball.update()
+        
+        racket1.update_r()
+        racket2.update_l() 
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.y > 500-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        #if ball.rect.x < 0: 
+           #finish = True
+          # window.blit(lose1,(200, 500))
+           #game_over = True 
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
     clock.tick(FPS)
     display.update()
